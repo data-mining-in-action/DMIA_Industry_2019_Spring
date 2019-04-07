c9cf6918779ac549b6cbc674c670a2a2017bacf5
tylorn <tylorn@yandex-team.ru>
2019-04-07 14:44:40 +0300

    hw05

---
+++ hw05/check.py
@@ -0,0 +1,32 @@
+import os
+import json
+import re
+from checker import Checker
+
+
+if __name__ == '__main__':
+    checker = Checker()
+    scores = {}
+    results = {}
+    folder_path = 'hw_data/significance/'
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.py'):
+            name, score = checker.check(folder_path + filename)
+            print(name, score)
+            if name is not None and score is not None:
+                print('score is', max(round(2 ** (6 * (score - 2.8)), 2), 0.05))
+                results[name] = score
+            else:
+                scores[name] = 0.05
+
+    best_accuracy = max(results.values())
+    for name in results:
+        scores[name] = max(round(2 ** (6 * (results[name] - 2.8)), 2), 0.05)
+
+    with open('hw_data/significance.json', 'w') as f:
+        json.dump(scores, f, indent=4)
+
+    with open('hw_data/significance.csv', 'w') as f:
+        f.write('email, score\n')
+        for name in sorted(scores):
+            f.write('{},{}\n'.format(name, scores[name]))
---
+++ hw05/checker.py
@@ -0,0 +1,85 @@
+import numpy as np
+import os
+import imp
+import signal
+import traceback
+import sys
+
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+LOW = 0.3
+HIGH = 0.8
+USERS_SIZE = 500
+CONVERSIONS_SIZE = 10000
+
+
+def signal_handler(signum, frame):
+    raise Exception("Timed out!")
+
+
+class Checker(object):
+    def __init__(self):
+        self.applications = 0
+
+    @staticmethod
+    def perform_test(random_gen, evaluate, effect):
+        train_user_probs = random_gen.uniform(low=LOW, high=HIGH, size=USERS_SIZE)
+        train_indices = random_gen.choice(USERS_SIZE, size=CONVERSIONS_SIZE, replace=True)
+        train_conversions = (
+            random_gen.uniform(size=CONVERSIONS_SIZE) < train_user_probs[train_indices]
+        ).astype(int)
+
+        test_user_probs = effect + random_gen.uniform(low=LOW, high=HIGH, size=USERS_SIZE)
+        test_indices = random_gen.choice(USERS_SIZE, size=CONVERSIONS_SIZE, replace=True)
+        test_conversions = (
+            random_gen.uniform(size=CONVERSIONS_SIZE) < test_user_probs[test_indices]
+        ).astype(int)
+
+        pvalue = evaluate(
+            train_conversions, 
+            train_indices,
+            test_conversions,
+            test_indices
+        )
+
+        return pvalue
+
+    @staticmethod
+    def eval_hits_ratio(random_gen, evaluate, effect, significance):
+        hits = 0
+        for _ in range(1000):
+            hits += Checker.perform_test(random_gen, evaluate, effect) < significance
+        return hits / 1000.
+
+
+    def check(self, script_path):
+        AUTHOR_EMAIL = None
+        random_gen = np.random.RandomState(42)
+        try:
+            signal.signal(signal.SIGALRM, signal_handler)
+            signal.alarm(120)
+            module = imp.load_source('significance_{}'.format(self.applications), script_path)
+            AUTHOR_EMAIL = module.AUTHOR_EMAIL
+            
+            correctness = self.eval_hits_ratio(random_gen, module.evaluate, effect=0., significance=0.05)
+            if correctness > 0.07:
+                raise ValueError('Incorrect test: hits raitio is {}'.format(correctness))
+
+            power_005 = self.eval_hits_ratio(random_gen, module.evaluate, effect=0.005, significance=0.05)
+            power_010 = self.eval_hits_ratio(random_gen, module.evaluate, effect=0.01, significance=0.05)
+            power_050 = self.eval_hits_ratio(random_gen, module.evaluate, effect=0.05, significance=0.05)
+
+            print(correctness, power_005, power_010, power_050)
+
+            return AUTHOR_EMAIL, power_050 + power_010 * 4 + power_005 * 16
+        except:
+            traceback.print_exception(*sys.exc_info())
+            return AUTHOR_EMAIL, None
+        finally:
+            self.applications += 1
+
+
+
+if __name__ == '__main__':
+    print(Checker().check(SCRIPT_DIR + '/example.py'))
